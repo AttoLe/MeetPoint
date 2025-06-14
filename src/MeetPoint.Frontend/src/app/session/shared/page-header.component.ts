e21b139dbd8e297e67988c7c46c3e141708c21c2
AttoLe <sergii.kucherenko.jr@gmail.com>
2025-06-14 03:50:22 +0300

    feat: added header subcomponents

---
+++ src/MeetPoint.Frontend/src/app/session/shared/page-header.component.ts
@@ -0,0 +1,30 @@
+import { Component, input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-page-header',
+  imports: [MatIconModule],
+  template: ` <div class="header-split">
+    <h1>Home</h1>
+    <div class="container clickable">
+      <ng-content></ng-content>
+    </div>
+  </div>`,
+  styles: `
+
+  .header-split {
+    margin: 0 10px 25px 10px;
+  }
+
+  .container {
+    display: flex;
+    gap: 0.75rem;
+  }
+
+  .clickable {
+      margin: auto 0;
+  }`,
+})
+export class SectionHeaderComponent {
+  title = input.required<string>();
+}
