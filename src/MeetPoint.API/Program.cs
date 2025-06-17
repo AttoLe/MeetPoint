7124faa356acc9add41c80b11345d232c4cb7798
AttoLe <sergii.kucherenko.jr@gmail.com>
2025-06-17 22:10:43 +0300

    feat: added signalr and redis initial setup

--- src/MeetPoint.API/Program.cs
+++ src/MeetPoint.API/Program.cs
@@ -1,11 +1,16 @@
 using MeetPoint.API;
 using MeetPoint.Application.Interfaces;
+using MeetPoint.Infrastructure.Cache;
 using MeetPoint.Infrastructure.Persistence;
 using MeetPoint.Infrastructure.Persistence.Entities;
 using MeetPoint.Infrastructure.Services;
+using MeetPoint.Infrastructure.SignalR;
+using MeetPoint.Infrastructure.Validators;
 
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +25,15 @@ builder.Services
 builder.Services
     .AddScoped<IFriendsService<string, IdentityUser>, FriendService>()
     .AddScoped<IFriendInvitationsService<string>, FriendInvitationService>();
+
+var redisConnection = builder.Configuration.GetConnectionString("Redis");
+builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+    ConnectionMultiplexer.Connect(redisConnection!));
+builder.Services.AddSingleton<IRedisService, RedisService>();
+
+builder.Services.AddSignalR()
+    .AddStackExchangeRedis(redisConnection!, options =>
+        { options.Configuration.ChannelPrefix = RedisChannel.Literal("SignalR"); });
 
 builder.Services.AddLocalSwaggerGen();
 builder.Services.AddControllers();
@@ -41,6 +55,10 @@ app.UseHttpsRedirection();
 app.UseRouting();
 
 app.MapGroup("/api/account/").MapIdentityApi<IdentityUser>();
+
+app.UseMiddleware<IdentityTokenQueryAuthenticationMiddleware>();
+app.MapHub<SessionHub>("/hubs/session");
+
 app.MapControllers();
 
 app.UseAuthentication();
---
+++ src/MeetPoint.Infrastructure/Validators/IdentityTokenQueryAuthMiddleware.cs
@@ -0,0 +1,47 @@
+using System.Security.Claims;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MeetPoint.Infrastructure.Validators;
+
+public class IdentityTokenQueryAuthenticationMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly IServiceProvider _serviceProvider;
+
+    public IdentityTokenQueryAuthenticationMiddleware(RequestDelegate next, IServiceProvider serviceProvider)
+    {
+        _next = next;
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var userManager = context.RequestServices.GetRequiredService<UserManager<IdentityUser>>();
+        if (!context.Request.Path.StartsWithSegments("/hubs"))
+        {
+            await _next(context);
+            return;
+        }
+
+
+        var userId = context.Request.Query["userId"].ToString();
+        var user = await userManager.FindByIdAsync(userId);
+
+        if (user != null)
+        {
+            var identity = new ClaimsIdentity("IdentityToken");
+            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id));
+            identity.AddClaim(new Claim(ClaimTypes.Name, user.UserName ?? ""));
+
+            var claimsPrincipal = new ClaimsPrincipal(identity);
+
+            context.User = claimsPrincipal;
+        }
+
+        await _next(context);
+    }
+}
